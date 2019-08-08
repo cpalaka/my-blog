052d89aca83ea3464c47261eb80a611c6a958bda
cpalaka <cpalaka@gmail.com>
2019-08-07 23:22:11 -0700

    rework blogpost component

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -3,37 +3,32 @@ import { Link } from 'react-router-dom'
 import Tag from '../Tag'
 import Hoverable from '../utils/Hoverable'
 // import './BlogPost.css'
-import './BlogPost1.css'
+import './BlogPost.css'
 
-const BlogPost1 = (props) => {
-    console.log(props)
+
+const BlogPost = (props) => {
+    const [isFull, setIsFull] = useState(false)
     return (
         <div className='blog-post'>
-            <div className="blog-date">{props.date}</div>
-            <div className='blog-header'>
-                <div className="blog-title">{props.title}</div>
-                <div className="blog-tags">{props.tags.map(t => <Tag name={t} key={t}>{t}</Tag>)}</div>
+            <Hoverable>
+                {s =>
+                <div className={`${ s ? 'grey' : ''}`}>
+                    <div className='showmore' onClick={()=>setIsFull(prev=>!prev)}>{isFull ?'-':'+'}</div>
+                    <div className='header'>
+                        
+                        <div className="title">{props.title.toUpperCase()}</div>
+                        <div className="date">{props.date}</div>
+                    </div>
+                    
+                </div>
+                }
+            </Hoverable>
+            <div className="tags">{props.tags.map((t,i) => (i!=0?'/':'')+t)}</div>
+            <div className={`post-preview ${isFull ? 'show' : 'hide'}`} dangerouslySetInnerHTML={{ __html: props.content }}>
             </div>
-            <div className="inner-blog">
-                {/* <Link to={'blog/' + props.url}></Link> */}
-                <div dangerouslySetInnerHTML={{ __html: props.content }} />
-            </div>
-            <div className="full-post-link"></div>
+
         </div>
     )
 }
-
-const BlogPost = (props) =>
-    <div className='blog-post'>
-        <Hoverable>
-            {s =>
-                <div className={`header ${ s ? 'red' : ''}`}>
-                    <div className="title">{props.title}</div>
-                    <div className="date">{props.date}</div>
-                </div>
-            }
-        </Hoverable>
-        <div className="tags">{props.tags.map(t => t+'/')}</div>
-    </div>
 
 export default BlogPost
