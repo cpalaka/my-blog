9f81fab2b7b926fab65c5e6bf66b09039f1c3346
cpalaka <cpalaka@gmail.com>
2019-09-17 15:53:26 -0700

    unfinished navmenu ui enhancements

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
 import Hoverable from '../utils/Hoverable'
@@ -9,19 +9,23 @@ import './Sidebar.css'
 
 const Sidebar = (props) => {
     console.log(props)
+    const path = props.location.pathname
+    const [aboutMenuOpened, setAboutMenuOpened] = useState(false)
+    const [blogMenuOpened, setBlogMenuOpened] = useState(false)
+
     return (
         <div className='sitelinks'>
-            
+
             <Hoverable>
                 {hovered =>
-                    <Link to='/about' style={{ textDecoration: 'none' }}>
+                    <div className={hovered || path === '/about' ? 'red-underline about' : 'about'}>
                         <img src={UserIcon} alt='user icon' />
-                    </Link>
+                    </div>
                 }
             </Hoverable>
             <Hoverable>
                 {hovered =>
-                    <Link to='/' style={{ textDecoration: 'none' }}>
+                    <Link to='/' className={hovered || path === '/' ? 'red-underline home' : 'home'}>
                         <img src={HomeIcon} alt='home icon' />
                     </Link>
 
@@ -29,9 +33,19 @@ const Sidebar = (props) => {
             </Hoverable>
             <Hoverable>
                 {hovered =>
-                    <Link to='/blog' style={{ textDecoration: 'none' }}>
-                        <img src={BlogIcon} alt='blog icon' />
-                    </Link>
+                    <>
+                    {blogMenuOpened ?
+                        <div className={'blog-submenu show'}>
+                            <Link to='/blog' style={{ textDecoration: 'none' }}>All</Link>
+                            <Link to='/blog/reviews' style={{ textDecoration: 'none' }}>Reviews</Link>
+                            <Link to='/blog/tech' style={{ textDecoration: 'none' }}>Tech</Link>
+                            <Link to='/blog/misc' style={{ textDecoration: 'none' }}>Misc</Link>
+                        </div> : <div className={'blog-submenu hide'}></div>
+                    }
+                        <div onClick={() => setBlogMenuOpened(t => !t)} className='blog'>
+                            <img src={BlogIcon} alt='blog icon' className={hovered || path === '/blog' ? 'red-underline' : ''} />
+                        </div>
+                    </>
                 }
             </Hoverable>
         </div>
